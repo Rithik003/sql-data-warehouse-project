3efc8efd8868b8920012ad2a3c0b6a56e33eb7ad
Rithik003 <130243349+Rithik003@users.noreply.github.com>
2026-02-14 16:23:39 +0530

    Update init_database.sql

--- scripts /init_database.sql
+++ scripts /init_database.sql
@@ -1,27 +1,27 @@
 /*
 ===============================================
-Create Databse and Schemas
+Create Databses
 ===============================================
 Script Purpose:
-  This script creates a new database named 'datawarehouse' after checking if it already exists. 
-  if the database exists,it is dropped and recreated ,additionally , the script sets up three schemas within the database :'broze','silver',and 'gold'.
+  This script creates a new database named 'dw_bronze','dw_silver','dw_gold' after checking if they already exists. 
+  if the databases exists,they are dropped and recreated.
 WARNING:
-  Running this script will drop the entire 'datawarehouse' database if it exists.
+  Running this script will drop the entire 'dw_bronze','dw_silver','dw_gold' if they exists.
   All  data in the database will be permently deleted .proceed with caution and ensure
   you have proper backups before running this script.alter
 */    
 
-# Drop and recreate the  'database' database 
-DROP DATABASE IF EXISTS datawarehouse;
+#CREATE DATABASES
 
+#-------BRONZE LAYER---------
+drop database if  exists dw_bronze;
+create database dw_bronze;
 
-# create database 
-create database datawarehouse;
+#-------SILVER LAYER---------
+drop database if  exists dw_silver;
+create database dw_silver;
 
-use datawarehouse;
+#-------GOLD LAYER------------
+drop database if  exists dw_gold;
+create database dw_gold;
 
-create schema bronze;
-
-create schema silver;
-
-create schema gold;
